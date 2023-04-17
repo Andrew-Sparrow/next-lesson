ef285be8d7780e5c8ce52c627e4649d34cab0d5d
Andrew-Sparrow <8911102@gmail.com>
2023-04-17 13:04:58 +0500

    adds Layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import Head from "next/head";
 import Script from "next/script";
+import Layout from "../../components/ProfileImage/Layout/layout";
 
 
 const FirstPost = () => {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -20,7 +21,7 @@ const FirstPost = () => {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
 
