f087b1f02c16be2869c509fd06865768e56c4a4c
Andrew-Sparrow <8911102@gmail.com>
2023-04-18 11:08:46 +0500

    adds detailed page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
 import Layout, { siteTitle } from '../components/layout/layout'
 import utilStyles from '../styles/utils.module.css'
+import Date from '@/components/date/date'
 import { getSortedPostsData } from '../lib/posts'
 
 export default function Home({ allPostsData }) {
@@ -12,16 +15,16 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <section className={`${ utilStyles.headingMd } ${ utilStyles.padding1px }`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${ id }`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
